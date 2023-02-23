e3354b17a7b7110b5764cb2d68e64016e0ca75bf
Otto Söderman <ottoj.soderman@gmail.com>
2023-02-23 19:11:35 +0200

    Added types and editing embed

--- src/gamequeue.py
+++ src/gamequeue.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import discord
 from discord import app_commands
 from .setup import client
@@ -6,8 +7,30 @@ import itertools
 queues = {}
 
 
+class Lobby:
+    def __init__(self, game: str, memberIds: list[int]):
+        self.game = game
+        self.memberIds = memberIds
+        self.memberCount = len(memberIds)
+
+    def __str__(self):
+        return f"{self.game}: {self.memberIds}"
+
+    def prettyString(self, queue: Queue):
+        memberUsernames: list[str] = []
+        for memberId in self.memberIds:
+            memberUsernames.append(queue.members[memberId].name)
+        return f"{self.game}: {memberUsernames}"
+
+    def __eq__(self, __o: object) -> bool:
+
+        if isinstance(__o, Lobby):
+            return self.game == __o.game and set(self.memberIds) == set(__o.memberIds)
+        return False
+
+
 class LobbyGroup:
-    def __init__(self, lobbies):
+    def __init__(self, lobbies: list[Lobby]):
         lobbies.sort(key=lambda lobby: lobby.game)
         self.lobbies = lobbies
 
@@ -21,10 +44,15 @@ class LobbyGroup:
         return list(map(lambda lobby: lobby.game, self.lobbies))
 
     def gamesString(self):
-        return "_".join(self.games())
+        return ", ".join(self.games())
 
     def __str__(self):
-        return f"{self.memberCount()}{list(map(lambda lobby: lobby.__str__(), self.lobbies))}"
+        lobbiesString = list(map(lambda lobby: lobby.__str__(), self.lobbies))
+        return f"{self.memberCount()}:{lobbiesString}"
+
+    def prettyString(self, queue: Queue):
+        lobbiesString = list(map(lambda lobby: lobby.prettyString(queue), self.lobbies))
+        return f"{self.memberCount()} players:\n{lobbiesString}"
 
     def __eq__(self, __o: object) -> bool:
         if isinstance(__o, LobbyGroup):
@@ -32,39 +60,23 @@ class LobbyGroup:
         return False
 
 
-class Lobby:
-    def __init__(self, game, members):
-        self.game = game
-        self.members = members
-        self.memberCount = len(members)
-
-    def __str__(self):
-        return f"{self.game}: {self.members}"
-
-    def __eq__(self, __o: object) -> bool:
-
-        if isinstance(__o, Lobby):
-            return self.game == __o.game and set(self.members) == set(__o.members)
-        return False
-
-
 class Queue:
-    def __init__(self, name, itemDict, messageId):
+    def __init__(self, name: str, itemDict: dict[str, int], messageId: int):
         self.name = name
-        self.members = {}
+        self.members: dict[int, QueueMember] = {}
         self.possibilities = []
         self.itemDict = itemDict
         self.messageId = messageId
 
-    def add_member(self, member):
+    def add_member(self, member: QueueMember):
         self.members[member.id] = member
 
-    def remove_member(self, id):
+    def remove_member(self, id: int):
         self.members.pop(id)
 
     def updatePossibilities(self):
-        possibilities = []
-        allPossibleLobbies = []
+        possibilities: list[LobbyGroup] = []
+        allPossibleLobbies: list[Lobby] = []
         for itemName, itemPlayerCount in self.itemDict.items():
             possibleMembers = list(
                 map(
@@ -78,18 +90,21 @@ class Queue:
             possibleCombinationsForGame = itertools.combinations(
                 possibleMembers, itemPlayerCount
             )
-            for comb in possibleCombinationsForGame:
+            comb: list[int]
+            for comb in possibleCombinationsForGame:  # type: ignore
                 lobby = Lobby(itemName, comb)
                 allPossibleLobbies.append(lobby)
 
-        def removeUnviableAndMakeGroup(lobby, lobbyGroup, remainingLobbies):
+        def removeUnviableAndMakeGroup(
+            lobby: Lobby, lobbyGroup: LobbyGroup, remainingLobbies: list[Lobby]
+        ):
             lobbies = lobbyGroup.lobbies.copy()
             lobbies.append(lobby)
             lobbyGroup = LobbyGroup(lobbies)
             filteredLobbies = list(
                 filter(
-                    lambda otherLobby: set(lobby.members).isdisjoint(
-                        otherLobby.members
+                    lambda otherLobby: set(lobby.memberIds).isdisjoint(
+                        otherLobby.memberIds
                     ),
                     remainingLobbies,
                 )
@@ -108,12 +123,12 @@ class Queue:
         possibilities.sort(
             key=lambda lobbyGroup: lobbyGroup.memberCount(), reverse=True
         )
-        uniquePossibilities = []
+        uniquePossibilities: list[LobbyGroup] = []
         for possibility in possibilities:
             if possibility not in uniquePossibilities:
                 uniquePossibilities.append(possibility)
 
-        possibilitiesGroupedByGame = {}
+        possibilitiesGroupedByGame: dict[str, list[LobbyGroup]] = {}
         for possibility in uniquePossibilities:
             games = possibility.gamesString()
             if games not in possibilitiesGroupedByGame:
@@ -123,13 +138,16 @@ class Queue:
         print(possibilities.__len__())
         print(uniquePossibilities.__len__())
 
+        onePossibilityPerGameGroup: list[LobbyGroup] = []
         for _, possibilitiesForSpesificGameGroup in possibilitiesGroupedByGame.items():
             print(possibilitiesForSpesificGameGroup[0])
+            onePossibilityPerGameGroup.append(possibilitiesForSpesificGameGroup[0])
+        return onePossibilityPerGameGroup
 
     def __str__(self):
         separator = ", "
-        itemsAsStrings = []
-        membersAsStrings = []
+        itemsAsStrings: list[str] = []
+        membersAsStrings: list[str] = []
         for value in self.itemDict.items():
             itemsAsStrings.append(value.__str__())
         for value in self.members.values():
@@ -138,15 +156,16 @@ class Queue:
 
 
 class QueueMember:
-    def __init__(self, name, id):
+    def __init__(self, name: str, id: int, joinIndex: int):
         self.name = name
         self.id = id
+        self.joinIndex = joinIndex
         self.items = []
 
-    def update_items(self, items):
+    def update_items(self, items: list[str]):
         self.items = items
 
-    def remove_item(self, index):
+    def remove_item(self, index: int):
         self.items.pop(index)
 
     def __str__(self):
@@ -154,45 +173,10 @@ class QueueMember:
         return f"{self.name}: [{separator.join(self.items)}]"
 
 
-testMembers = []
-
-testGames = [
-    ["LoL"],
-    ["LoL", "CSGO"],
-    ["LoL", "CSGO", "Valorant"],
-    ["LoL", "CSGO", "Overwatch"],
-    ["Lol", "CSGO", "Valorant", "Overwatch"],
-    ["Lol", "CSGO", "Valorant", "Overwatch"],
-    ["LoL", "CSGO"],
-    ["LoL", "CSGO"],
-    ["LoL", "CSGO"],
-    ["Lol"],
-    ["Lol"],
-    ["CSGO"],
-    ["CSGO"],
-    ["CSGO"],
-    ["CSGO"],
-    ["Valorant"],
-    ["Valorant"],
-    ["Valorant"],
-    ["Overwatch"],
-    ["Overwatch"],
-    ["Overwatch"],
-    ["Overwatch"],
-    ["Overwatch"],
-]
-
-for i in range(0, testGames.__len__() - 1):
-    testMember = QueueMember(f"Test{i}", i)
-    items = testGames[i]
-    testMember.update_items(items)
-    testMembers.append(testMember)
-
-
-class Select(discord.ui.Select):
+class Select(discord.ui.Select):  # type: ignore
     def __init__(self, queue: Queue):
         self.queueName = queue.name
-        optionElems = []
+        optionElems: list[discord.SelectOption] = []
         for key, _ in queue.itemDict.items():
             optionElems.append(discord.SelectOption(label=key))
         super().__init__(
@@ -203,16 +187,25 @@ class Select(discord.ui.Select):
         )
 
     async def callback(self, interaction: discord.Interaction):
-        queue = queues[self.queueName]
+        queue: Queue = queues[self.queueName]
         user = interaction.user
         if user.id not in queue.members:
-            queue.add_member(QueueMember(user.name, user.id))
+            queue.add_member(QueueMember(user.name, user.id, queue.members.__len__()))
         queueMember = queue.members[user.id]
         queueMember.update_items(self.values)
-        channel = interaction.channel
-        message = await channel.fetch_message(queue.messageId)
-        embed = message.embeds[0]
-        embed.description = queue.__str__()
+        channel: discord.TextChannel = interaction.channel  # type: ignore
+        message: discord.Message = await channel.fetch_message(queue.messageId)
+        oldEmbed = message.embeds[0]
+        embed = discord.Embed(title=oldEmbed.title, color=0xFF4500)
+        possibleLobbies = queue.updatePossibilities()
+        for lobbyGroup in possibleLobbies:
+            embed.add_field(
+                name=lobbyGroup.gamesString(),
+                value=lobbyGroup.prettyString(queue),
+                inline=False,
+            )
+
+        queue.updatePossibilities()
         await message.edit(embed=embed)
         await interaction.response.defer()
 
@@ -234,8 +227,8 @@ async def queue(interaction: discord.Interaction, name: str, items: str):
     for item in items.split(","):
         key, value = item.split(":")
         itemsDict[key] = int(value)
-    res = await interaction.channel.send(embed=embed)
-    queue = Queue(name, itemsDict, res.id)
+    res = await interaction.channel.send(embed=embed)  # type: ignore
+    queue = Queue(name, itemsDict, res.id)  # type: ignore
     for member in testMembers:
         queue.add_member(member)
     queue.updatePossibilities()
@@ -246,7 +239,42 @@ async def queue(interaction: discord.Interaction, name: str, items: str):
     )
 
 
-queue = Queue("Cool", {"LoL": 10, "CSGO": 10, "Overwatch": 6, "Valorant": 5}, 1)
+testMembers: list[QueueMember] = []
+
+testGames = [
+    ["LoL"],
+    ["LoL", "CSGO"],
+    ["LoL", "CSGO", "Valorant"],
+    ["LoL", "CSGO", "Overwatch"],
+    ["LoL", "CSGO", "Valorant", "Overwatch"],
+    ["LoL", "CSGO", "Valorant", "Overwatch"],
+    ["LoL", "CSGO"],
+    ["LoL", "CSGO"],
+    ["LoL", "CSGO"],
+    ["LoL"],
+    ["LoL"],
+    ["CSGO"],
+    ["CSGO"],
+    ["CSGO"],
+    ["CSGO"],
+    ["Valorant"],
+    ["Valorant"],
+    ["Valorant"],
+    ["Overwatch"],
+    ["Overwatch"],
+    ["Overwatch"],
+    ["Overwatch"],
+    ["Overwatch"],
+]
+
+for i in range(0, testGames.__len__() - 1):
+    testMember = QueueMember(f"Gamer{i}", i, i)
+    items = testGames[i]
+    testMember.update_items(items)
+    testMembers.append(testMember)
+
+
+queue: Queue = Queue("Cool", {"LoL": 10, "CSGO": 10, "Overwatch": 6, "Valorant": 5}, 1)  # type: ignore
 for member in testMembers:
     queue.add_member(member)
 queue.updatePossibilities()
